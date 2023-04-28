9df38ee27aa366c2bf75d5b72d1f7e4db5cf9803
chemoskabron <chemoskabron@gmail.com>
2023-04-28 16:19:11 -0700

    3er commit

---
+++ Variables_tipos_de_datos_y_metodos_de_entrada.py
@@ -0,0 +1,17 @@
+# numero1 = 9
+# numero2 = 8
+# resultado = numero1+numero2
+# print(resultado)
+
+# numero1 = float(input('ingresa un número '))
+# numero2 = float9(input('ingresa un número '))
+
+# resultado = numero1+numero2
+# print(resultado)
+
+# verdadero = True
+# falso = False
+
+
+
+print(8>9)
---
+++ operadores_basicos.py
@@ -0,0 +1,24 @@
+"""
+OPERADORES ARITMETICOS
+"""
+numero1 = float(input('ingresa un número '))
+numero2 = float(input('ingresa un número '))
+
+# print(f'suma {numero1 + numero2}')
+# print(f'resta {numero1 - numero2}')
+# print(f'multiplicación {numero1 * numero2}')
+# print(f'división {numero1 / numero2}')
+# print("modulo ",(numero1 % numero2))
+# print("potencia ",(numero1 ** numero2))
+# print("división exacta ",(numero1 // numero2))
+
+"""
+OPERADORES RELACIONALES
+"""
+
+print(" > ", numero1 > numero2)
+print(" < ", numero1 < numero2)
+print(" >= ", numero1 >= numero2)
+print(" <= ", numero1 <= numero2)
+print(" == ", numero1 == numero2)
+print(" != ", numero1 != numero2)
