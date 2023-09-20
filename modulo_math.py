2404038e22a9c83fe5ddc02a5d7cee144ce7af02
Guillermo-Guzman <chemoskabron@gmail.com>
2023-09-19 20:58:37 -0700

    primer commit

---
+++ modulo_math.py
@@ -0,0 +1,7 @@
+import math
+
+numero = int(input('ingresa un numero por favor '))
+print(f'Raiz de {numero} = {round(math.sqrt(numero))}')
+print(f'Seno de {numero} = {math.sin(numero)}')
+print(f'Exponencial e de {numero} = {math.exp(numero)}')
+print(f'numero {numero} = {math.log(math.exp(numero))}')
---
+++ operadores_basicos.py
@@ -0,0 +1,38 @@
+"""
+OPERADORES ARITMETICOS
+"""
+# numero1 = float(input('ingresa un número '))
+# numero2 = float(input('ingresa un número '))
+
+# print(f'suma {numero1 + numero2}')
+# print(f'resta {numero1 - numero2}')
+# print(f'multiplicación {numero1 * numero2}')
+# print(f'división {numero1 / numero2}')
+# print("modulo ",(numero1 % numero2))
+# print("potencia ",(numero1 ** numero2))
+# print("división exacta ",(numero1 // numero2))
+
+"""
+OPERADORES RELACIONALES
+"""
+
+# print(" > ", numero1 > numero2)
+# print(" < ", numero1 < numero2)
+# print(" >= ", numero1 >= numero2)
+# print(" <= ", numero1 <= numero2)
+# print(" == ", numero1 == numero2)
+# print(" != ", numero1 != numero2)
+
+"""
+OPERADORES DE ASIGNACION
+"""
+# numero3 = 8
+# numero3 = numero3 - 2
+# print(numero3)
+
+"""
+OPERADORES LOGICOS
+"""
+
+#and, or, not
+numero = True
